81244894c12a9a59b1e0ce48f876653761e167ba
Johannes Tax <johannes.tax@gmx.at>
2016-01-06 19:41:03 +0100

    Fixes #14: Make console correctly displaying Commands.

--- src/shellscript/console.py
+++ src/shellscript/console.py
@@ -1,4 +1,5 @@
 import code
+import codeop
 import sys
 
 from shellscript.settings import prompt_1, prompt_2
@@ -7,19 +8,34 @@ from shellscript.settings import prompt_1, prompt_2
 def interactive_output(obj):
     if isinstance(obj, shellscript.proto.Command):
         obj.interact()
-    return None
-
-
-def readfunc(prompt=''):
-    if prompt == sys.ps2:
-        prompt = prompt_2()
+        return None
     else:
-        prompt = prompt_1()
-    return 'interactive_output(%s)' % input(prompt).strip()
+        return obj
+
+
+class Read(object):
+
+    def __init__(self):
+        self.compile = codeop.CommandCompiler()
+
+    def __call__(self, prompt=''):
+        if prompt == sys.ps2:
+            return input(prompt_2())
+        else:
+            line = input(prompt_1())
+            modline = 'interactive_output(%s)' % line.strip()
+            try:
+                self.compile(modline)
+                if '=' in line.split('(')[0]:
+                    raise SyntaxError()
+            except SyntaxError:
+                return line
+            else:
+                return modline
 
 
 if __name__ == '__main__':
     import shellscript
     local = shellscript.__dict__
     local['interactive_output'] = interactive_output
-    code.interact(readfunc=readfunc, local=local)
+    code.interact(readfunc=Read(), local=local)
